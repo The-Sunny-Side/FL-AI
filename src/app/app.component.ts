2ba5615135497930d57d439e76da5b3d8c780ee7
Dazi816 <davide.folino816@gmail.com>
2025-02-19 01:19:30 +0100

    add constants and responsive dimensions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { COLORS } from './utils/constants/colors';
+import { FONT } from './utils/constants/font';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 })
 export class AppComponent {
   title = 'FL-AI';
+
+  constructor() {
+    //colors
+    document.documentElement.style.setProperty('--azzurro', COLORS.AZZURRO);
+    document.documentElement.style.setProperty('--arancione', COLORS.ARANCIONE);
+    //fonts
+    document.documentElement.style.setProperty('--font-size-logo', FONT.FONT_SIZE_LOGO);
+
+  }
 }
---
+++ src/app/utils/constants/font.ts
@@ -0,0 +1,3 @@
+export const FONT = {
+    FONT_SIZE_LOGO: '1.5rem'
+  };
